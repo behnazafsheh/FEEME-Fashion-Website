aa0a0302175ca78e725e48c2c66db85b6e3c4f5a
behnazafsheh <behnazfaheh1992@gmail.com>
2022-07-06 12:55:12 +0430

    hero section added

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const menuBox = document.querySelectorAll(".form");
-menuBox.forEach(elem => {
-    elem.addEventListener('click', () => {
-        for (let i = 0; i < menuBox.length; i++) {
-            menuBox[i].classList.remove("active");
-        }
-        elem.classList.add("active");
-    })
-    
-})
+menuBox.forEach((elem) => {
+  elem.addEventListener("click", () => {
+    for (let i = 0; i < menuBox.length; i++) {
+      menuBox[i].classList.remove("active");
+    }
+    elem.classList.add("active");
+  });
+});
